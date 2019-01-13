b7573439ce26c09831a138c50ac108f55216200d
aidansun525 <42677795+aidansun525@users.noreply.github.com>
2019-01-13 14:04:20 -0800

    Create Main.java

---
+++ Homework/Palindrome Tester/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+    
+        output("Palindrome");
+        output("Civic");
+        output("Radar");
+        output("Level");
+        output("Eva, can I stab bats in a cave?");
+        output("Mr. Owl ate my metal worm");
+        output("Never odd or even");
+        output("Doc, Note: I Dissent. A fast never prevents a fatness. I diet on cod.");
+    }
+
+    public static void output(String s){
+        if(testPal(s)){
+            System.out.println("True!  " + "\"" + s + "\"" + " is a palindrome!");
+        }
+        else{
+            System.out.println("False!  " + "\""+ s + "\"" + " is not a palindrome!");
+        }
+    }
+
+    public static boolean testPal(String s){
+        String ignoreAll = s.replaceAll("[\\W]","");
+        int len = ignoreAll.length();
+        boolean palindrome = true;
+        int n = 1;
+        
+        do {
+            if(palindrome) {
+                String sub1 = ignoreAll.substring(len - n, len - n + 1);
+                String sub2 = ignoreAll.substring(n - 1, n);
+                palindrome = sub1.equalsIgnoreCase(sub2);
+            }
+            n++;
+        } while (n<=(int)(len/2.0));
+        
+        return palindrome;
+    }
+}
