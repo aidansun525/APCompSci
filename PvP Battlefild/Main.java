eb45a54de0b60f92052666243454c010c422bef7
aidansun525 <42677795+aidansun525@users.noreply.github.com>
2019-03-26 08:08:49 -0700

    Update Main.java

--- PvP Battlefild/Main.java
+++ PvP Battlefild/Main.java
@@ -3,23 +3,34 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
-        int val = 5;
+        
         Random num = new Random();
 
         Fighter player = new Fighter("f");
         player.showStats();
+        
+        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
 
         Priest player2 = new Priest("pr");
         player2.showStats();
+        
+        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
 
         Mage player3 = new Mage("m");
         player3.showstats();
+        
+        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
 
         Paladin player4 = new Paladin("pa");
         player4.showstats();
+        
+        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
 
-        PlayerCharacter playerx = new PlayerCharacter(); 
+        ArrayList<PlayerCharacter> players = new ArrayList<PlayerCharacter>();
+        players.add(new Fighter ("f"));
+        players.add(new Paladin("pa"));
+        players.add(new Priest("pr"));
+        players.add(new Mage("m"));
 
     }
 }
