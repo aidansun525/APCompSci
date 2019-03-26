8e3786c22cd304861944a9b29943a62263b08cb4
aidansun525 <42677795+aidansun525@users.noreply.github.com>
2019-03-26 08:16:17 -0700

    Update Main.java

--- PvP Battlefild/Main.java
+++ PvP Battlefild/Main.java
@@ -1,36 +1,23 @@
-import java.util.Random;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        
-        Random num = new Random();
-
-        Fighter player = new Fighter("f");
-        player.showStats();
-        
-        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
-
-        Priest player2 = new Priest("pr");
-        player2.showStats();
-        
-        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
-
-        Mage player3 = new Mage("m");
-        player3.showstats();
-        
-        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
-
-        Paladin player4 = new Paladin("pa");
-        player4.showstats();
-        
-        System.out.println("There are, "+PlayerCharacter.numCharacters()+" characters\n");
 
         ArrayList<PlayerCharacter> players = new ArrayList<PlayerCharacter>();
-        players.add(new Fighter ("f"));
-        players.add(new Paladin("pa"));
-        players.add(new Priest("pr"));
-        players.add(new Mage("m"));
+        players.add(new Fighter("Apple"));
+        players.add(new Priest("Banana"));
+        players.add(new Paladin("Coconut"));
+        players.add(new Mage("Durian"));
+
+        System.out.println(players.size());
+
+        for (PlayerCharacter x : players) {
+            if (x instanceof Priest){
+                System.out.println("Here is a Priest.");
+                x.showStats();
+            }
+        }
 
     }
 }
